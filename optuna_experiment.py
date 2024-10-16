345d3cd7815ddb6ad544693f6176f4ab14ec2d17
Thomas Wolgast <thomas.wolgast@uni-oldenburg.de>
2024-10-16 14:42:54 +0200

    Add objective and HP sampling for safe RL algorithm USL

--- optuna_experiment.py
+++ optuna_experiment.py
@@ -38,6 +38,36 @@ def custom_multi_obj_metric(eval_metrics):
     return invalid_share, valid_regret
 
 
+class custom_single_obj_metric_safe():
+    def __init__(self,
+                 env_name='default',
+                 min_valid_share=0.999,
+                 valid_offset=1):
+        normalization = {  # Re-used parameters from objective normalization
+            'default': 1.0,
+            'qmarket': 0.212,
+            'voltage': 0.113,
+            'load': 7.14,
+            'eco': 24.21,
+            'renewable': 7.3}
+        self.normalization = normalization[env_name]
+        self.min_valid_share = min_valid_share
+        self.valid_offset = valid_offset
+
+    def __call__(self, eval_metrics):
+        valid_share = np.mean([m['valid_share_possible'] for m in eval_metrics])
+        invalid_share = 1 - valid_share
+        if valid_share < self.min_valid_share:
+            return invalid_share
+
+        # Only consider regret if constraint satisfaction is high enough
+        regret = np.mean([m['regret'] for m in eval_metrics])
+        # Normalize regret with experience from old experiments (otherwise regret far too dominating or too weak)
+        regret /= self.normalization
+
+        return invalid_share + regret - self.valid_offset
+
+
 def opf_env_design_sampling(trial):
     # Define hyperparameters to optimize
     hps = dict()
@@ -130,16 +160,98 @@ def opf_env_design_sampling(trial):
     return hps
 
 
+def opf_env_design_sampling_safe(trial):
+    # Focus only on the design decisions relevant for the cost function for safe RL
+    hps = dict()
+
+    hps['only_worst_case_violations'] = trial.suggest_categorical('only_worst_case_violations', [True, False])
+
+    # Penalty function
+    hps['penalty_fct'] = trial.suggest_categorical('penalty_fct', ['linear', 'quadr', 'sqrt'])
+    linear_penalty = 0
+    quadr_penalty = 0
+    sqrt_penalty = 0
+    if hps['penalty_fct'] == 'linear':
+        linear_penalty = 1
+    elif hps['penalty_fct'] == 'quadr':
+        quadr_penalty = 1
+    elif hps['penalty_fct'] == 'sqrt':
+        sqrt_penalty = 1
+    # Simplification: Same setting for all constraints!
+    hps['volt_pen_kwargs'] = {'linear_penalty': linear_penalty, 'quadr_penalty': quadr_penalty, 'sqrt_penalty': sqrt_penalty}
+    hps['trafo_pen_kwargs'] = {'linear_penalty': linear_penalty, 'quadr_penalty': quadr_penalty, 'sqrt_penalty': sqrt_penalty}
+    hps['line_pen_kwargs'] = {'linear_penalty': linear_penalty, 'quadr_penalty': quadr_penalty, 'sqrt_penalty': sqrt_penalty}
+    hps['ext_grid_pen_kwargs'] = {'linear_penalty': linear_penalty, 'quadr_penalty': quadr_penalty, 'sqrt_penalty': sqrt_penalty}
+
+    # Reward function params
+    # Valid reward not possible here because the cost function must not flip sign
+    # hps['valid_reward'] = trial.suggest_float('valid_reward', 0.0, 2.0)
+    hps['invalid_penalty'] = trial.suggest_float('invalid_penalty', 0.0, 2.0)
+    # hps['objective_share'] = trial.suggest_float('objective_share', 0.0, 1.0)
+    hps['reward_function_params'] = dict()
+    # hps['reward_function_params']['valid_reward'] = hps['valid_reward']
+    hps['reward_function_params']['invalid_penalty'] = hps['invalid_penalty']
+    # hps['reward_function_params']['objective_share'] = hps['objective_share']
+
+    return hps
+
+
+def opf_algo_hp_sampling_safe(trial):
+    # Sample HPs for the safe RL algorithm
+    hps = dict()
+
+    # USL-SAC-specific HPs
+    hps['penalty_factor'] = trial.suggest_float('penalty_factor', 0.5, 5, log=True)
+    hps['max_projection_iterations'] = trial.suggest_int('max_projection_iterations', 1, 20)
+    hps['extra_cost_critic'] = trial.suggest_categorical('extra_cost_critic', [True, False])
+    # hps['use_correction_optimizer'] = trial.suggest_categorical('use_correction_optimizer', [True, False])
+
+    # Specific SAC HPs
+    hps['entropy_learning_rate'] = trial.suggest_float('entropy_learning_rate', 0.00005, 0.01, log=True)
+    hps['target_entropy'] = trial.suggest_int('target_entropy', -50, 0)
+
+    # General HPs
+    batch_sizes = [64, 128, 256, 512, 1024, 2048]
+    batch_size_index = trial.suggest_int('batch_size_index', 0, 5)
+    hps['batch_size'] = batch_sizes[batch_size_index]
+    hps['start_train'] = int(hps['batch_size'] * 1.5)
+
+    hps['actor_learning_rate'] = trial.suggest_float('actor_learning_rate', 0.00005, 0.01, log=True)
+    hps['critic_learning_rate'] = trial.suggest_float('critic_learning_rate', 0.00005, 0.01, log=True)
+
+    train_intervals = (
+        {'train_interval': 1, 'train_steps': 2},
+        {'train_interval': 1, 'train_steps': 1},
+        {'train_interval': 2, 'train_steps': 1},
+        {'train_interval': 3, 'train_steps': 1},
+    )
+    train_interval_index = trial.suggest_int('train_interval_index', 0, 3)
+    hps.update(train_intervals[train_interval_index])
+
+    actor_net_layers = trial.suggest_int('actor_net_layers', 1, 4)
+    actor_net_neurons = [128, 256, 512]
+    actor_net_neuron_index = trial.suggest_int('actor_net_size_index', 0, 2)
+    hps['actor_fc_dims'] = [actor_net_neurons[actor_net_neuron_index]] * actor_net_layers
+
+    critic_net_layers = trial.suggest_int('critic_net_layers', 1, 4)
+    critic_net_neurons = [128, 256, 512]
+    critic_net_neuron_index = trial.suggest_int('critic_net_size_index', 0, 2)
+    hps['critic_fc_dims'] = [critic_net_neurons[critic_net_neuron_index]] * critic_net_layers
+
+    return hps
+
+
 # Multi-objective: optuna.samplers.NSGAIIISampler() or default (None)
 # Single_objective: optuna.samplers.GPSampler(n_startup_trials=10)
 main(sampler=None,
-        env_hp_sampling_method=opf_env_design_sampling,
-        metric=custom_multi_obj_metric,
+        env_hp_sampling_method=opf_env_design_sampling_safe,
+        hp_sampling_method=opf_algo_hp_sampling_safe,
+        metric=custom_single_obj_metric_safe(),
         storage=True,
         load_if_exists=True,
-        directions=["minimize", "minimize"],
-        # direction='minimize',
+        # directions=["minimize", "minimize"],
+        direction='minimize',
         n_seeds=3,
-        mean=True,
-        last_n_steps=4)
+        median=True,
+        last_n_steps=3)
         # pruner=optuna.pruners.PercentilePruner(50.0, n_startup_trials=10, n_warmup_steps=1))
