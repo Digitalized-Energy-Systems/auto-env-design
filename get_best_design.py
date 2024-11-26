94039e36c2a252091cf02ffc7ea8fce72181b853
Thomas Wolgast <thomas.wolgast@uni-oldenburg.de>
2024-11-26 17:24:58 +0100

    Add annotated pareto front plot + make code more modular

--- get_best_design.py
+++ get_best_design.py
@@ -27,12 +27,12 @@ are multiple objectives to consider. Variants defined here are:
 - constraints: Look at top x solutions regarding constraint satisfaction
 - objective: Look at top x solutions regarding objective optimization
 """
-VARIANTS = ('pareto, fuzzy_pareto, constraints, objective', 'utopia')
-VARIANT = 'utopia'
+VARIANTS = ('pareto', 'fuzzy_pareto', 'constraints', 'objective', 'utopia')
+VARIANT = 'fuzzy_pareto'
 LOOK_AT_TOP_N = 10
 
 
-def main(paths, env_names):
+def main(paths, env_names, variant, store=True):
 
     top_designs = []
     bottom_designs = []
@@ -55,49 +55,61 @@ def main(paths, env_names):
         df = df[df.state == 'COMPLETE']
         df = df.dropna(subset=metric_names)
         df.reset_index(drop=True, inplace=True)
-        df['not_dominated'] = pareto_front.compute_pareto_front(df[metric_names])
-        df['fuzzy_dominated'] = pareto_front.compute_fuzzy_dominance(df[metric_names], df['not_dominated'], epsilon)
+        df['non_dominated'] = pareto_front.compute_pareto_front(df[metric_names])
+        df['fuzzy_dominated'] = pareto_front.compute_fuzzy_dominance(df[metric_names], df['non_dominated'], epsilon)
         full_df = df.copy()
 
-        if VARIANT == 'pareto':
+        print('Use evaluation: ', variant)
+        if variant == 'pareto':
             top_df, bottom_df = filter_by_pareto(df)
-        elif VARIANT == 'fuzzy_pareto':
+        elif variant == 'fuzzy_pareto':
             top_df, bottom_df = filter_by_fuzzy_pareto(df)
-        elif VARIANT == 'constraints':
+        elif variant == 'constraints':
             top_df, bottom_df = filter_by_constraints(df, LOOK_AT_TOP_N)
-        elif VARIANT == 'objective':
+        elif variant == 'objective':
             top_df, bottom_df = filter_by_objective(df, LOOK_AT_TOP_N)
-        elif VARIANT == 'utopia':
+        elif variant == 'utopia':
             top_df, bottom_df = filter_by_utopia(df, LOOK_AT_TOP_N)
         else:
-            raise ValueError(f'Unknown variant: {VARIANT}')
+            raise ValueError(f'Unknown variant: {variant}')
 
+        # alternative_statistical_significance(full_df, metric_names)
 
-        best_design = get_best_design(top_df, bottom_df)
-        pprint_best_design(best_design, env_names[idx])
+        best_design_continuous, best_design_discrete = get_best_design(top_df, bottom_df)
+        pprint_best_design(best_design_continuous, best_design_discrete, env_names[idx])
+
+        if store:
+            # Store the design distribution to csv
+            best_design_continuous.to_csv(f'{path}/best_design_continuous_{variant}.csv')
+            best_design_discrete.to_csv(f'{path}/best_design_discrete_{variant}.csv')
 
         top_designs.append(top_df)
         bottom_designs.append(bottom_df)
 
     # Trow all environments together (what is the best env design over all problems?)
-    print(f'Get overall best design regardign {VARIANT}')
-    total_df = pd.concat(top_designs)
+    print(f'Get overall best design regarding {variant}')
+    best_design_continuous, best_design_discrete = get_best_design(
+        pd.concat(top_designs), pd.concat(bottom_designs))
+    pprint_best_design(best_design_continuous, best_design_discrete, name='Overall best design')
 
-    total_best_design = get_best_design(total_df, pd.concat(bottom_designs))
-    pprint_best_design(total_best_design, 'Overall best design')
+    print('---------------------------------')
+
+    if store:
+        one_path_lower = os.path.split(path)[0]
+        best_design_continuous.to_csv(f'{one_path_lower}/best_design_continuous_{variant}.csv')
+        best_design_discrete.to_csv(f'{one_path_lower}/best_design_discrete_{variant}.csv')
 
 
-def filter_by_pareto(df):
-    top_df = df[df.not_dominated]
-    bottom_df = df[~df.not_dominated]
-    print('N Non-dominated: ', len(top_df.index.values))
+
+def filter_by_pareto(df) -> tuple[pd.DataFrame, pd.DataFrame]:
+    top_df = df[df.non_dominated]
+    bottom_df = df[~df.non_dominated]
     return top_df, bottom_df
 
 def filter_by_fuzzy_pareto(df):
-    flag = df.fuzzy_dominated + df.not_dominated
+    flag = df.fuzzy_dominated + df.non_dominated
     top_df = df[flag]
     bottom_df = df[~flag]
-    print('N Non-dominated: ', len(top_df.index.values))
     return top_df, bottom_df
 
 def filter_by_constraints(df, best_n=10):
@@ -119,49 +131,102 @@ def filter_by_utopia(df, best_n=10):
     return df.head(best_n), df.iloc[best_n:]
 
 
-def get_best_design(df, bottom_df=None):
+def get_best_design(df, bottom_df):
     print('Number of considered designs: ', len(df))
-    best_design = []
+    best_design_continuous = []
+    best_design_discrete = []
     design_decisions = [c for c in df.columns if "params_" in c]
+
     for param in design_decisions:
         # Compute mean and std for continuous values
         if isinstance(df[param].iloc[0], float):
-            # print(f'{param}: {df[param].mean()} +- {df[param].std()}')
             overutilization = df[param].mean() / bottom_df[param].mean()
             # Test for statistical significance (is the distribution different from the overall distribution?)
-            t, p = ttest_ind(df[param], bottom_df[param], equal_var=False)  # assumption: unequal variance
-            best_design.append((param, df[param].mean(), df[param].std(), overutilization, f'p-value:', p))
+            t, p_value = ttest_ind(df[param], bottom_df[param], equal_var=False)  # assumption: unequal variance
+            data = {'Parameter': param, 'Mean': df[param].mean(), 'Std': df[param].std(), 'Overutilization': overutilization, 'p-value': p_value}
+            best_design_continuous.append(data)
         else:
-            # If boolean or string: return most used entry instead
-            # print(f'{param}: {df[param].mode()[0]} ({df[param].value_counts().iloc[0]}/{len(df)})')
+            # If boolean, string or int: return most used entry instead
             most_used_category = df[param].mode()[0]
             share_of_most_used = df[param].value_counts().iloc[0]/len(df)
-            share_overall = bottom_df[param].value_counts()[most_used_category]/len(bottom_df)
-            overutilization = share_of_most_used / share_overall
+            try:
+                share_overall = bottom_df[param].value_counts()[most_used_category]/len(bottom_df)
+                overutilization = share_of_most_used / share_overall
+            except KeyError:
+                # most_used_category is not in bottom_df
+                overutilization = np.inf
             # Test for statistical significance
             contingency_table = pd.DataFrame({
                 'Top Group': df[param].value_counts(),
                 'Rest Group': bottom_df[param].value_counts()
             }).fillna(0)
             chi2, p_value, _, _ = chi2_contingency(contingency_table)
-            best_design.append((param, most_used_category, share_of_most_used, overutilization, f'p-value:', p_value))
+            data = {'Parameter': param, 'Most Used': most_used_category, 'Share': share_of_most_used, 'Overutilization': overutilization, 'p-value': p_value}
+            best_design_discrete.append(data)
 
-    return best_design
+    best_design_continuous = pd.DataFrame(best_design_continuous)
+    best_design_discrete = pd.DataFrame(best_design_discrete)
+
+    for df in (best_design_continuous, best_design_discrete):
+        df['Significant'] = df['p-value'] < 0.05
+
+    return best_design_continuous, best_design_discrete
 
 
-def pprint_best_design(best_design, name='Best design', min_overutilization=0.3):
-    print('---------------------------------')
+def pprint_best_design(best_design_continuous, best_design_discrete, name='Best design'):
     print(name, ':')
-    for design_decision in best_design:
-        if design_decision[-1] < 0.05:
-            print(design_decision)
-        else:
-            print(f'Skip: {design_decision[0]}')
+    print('Continuous:')
+    print(best_design_continuous)
+    print('Discrete:')
+    print(best_design_discrete)
+    total = len(best_design_continuous) + len(best_design_discrete)
+    significance = best_design_continuous['Significant'].sum() + best_design_discrete['Significant'].sum()
+    mean_significance = significance / total
+    print('Overall significance: ', mean_significance)
     print('---------------------------------')
+
+
+def alternative_statistical_significance(df, metrics):
+    """ The test above checks if the top x designs are significantly different
+    from the rest, however, without actually looking at the performance metrics,
+    except using them for the split.
+    Here, we perform the group split based on the design decisions and then
+    look if the performance metrics are significantly different. """
+    design_decisions = [c for c in df.columns if "params_" in c]
+    for metric in metrics:
+        continuous_design_decisions = []
+        discrete_design_decisions = []
+        for param in design_decisions:
+            if isinstance(df[param].iloc[0], float):
+                # Continuous
+                top_50_percentile = df[param].quantile(0.5)
+                top_df = df[df[param] > top_50_percentile]
+                rest_df = df[df[param] <= top_50_percentile]
+                # Test for statistical significance
+                t, p_value = ttest_ind(top_df[metric], rest_df[metric], equal_var=False)
+                data = {'Parameter': param[7:], 'Mean': df[param].mean(), 'Std': df[param].std(), 'p-value': p_value, 'Significant': p_value < 0.05}
+                continuous_design_decisions.append(data)
+            else:
+                # Discrete
+                most_used_category = df[param].mode()[0]
+                most_used_df = df[df[param] == most_used_category]
+                rest_df = df[df[param] != most_used_category]
+                # Test for statistical significance
+                t, p_value = ttest_ind(most_used_df[metric], rest_df[metric], equal_var=False)
+                data = {'Parameter': param[7:], 'Most Used': most_used_category, 'p-value': p_value, 'Significant': p_value < 0.05}
+                discrete_design_decisions.append(data)
+
+        print(metric)
+        print('Continuous:')
+        print(pd.DataFrame(continuous_design_decisions))
+        print('Discrete:')
+        print(pd.DataFrame(discrete_design_decisions))
+        print('---------------------------------')
 
 
 if __name__ == '__main__':
-    env_names = ('eco', 'load', 'renewable', 'voltage', 'qmarket')
-    paths = [f'HPC/auto_env_design/data/20240906_{env_name}_multi' for env_name in env_names]
+    env_names = ('voltage', 'qmarket', 'load')  # 'eco', 'load', 'renewable', 
+    paths = [f'HPC/auto_env_design/data/20241121_multi_GA_full/{env_name}' for env_name in env_names]
 
-    main(paths, env_names)
+    for variant in VARIANTS:
+        main(paths, env_names, variant)
--- plot_pareto_plus.py
+++ plot_pareto_plus.py
@@ -4,7 +4,12 @@ experiments for comparison. """
 import os
 
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+import pandas as pd
+import tikzplotlib
 
+import get_best_design
 import pareto_front
 from util import load_optuna_csv, load_drl_csv
 
@@ -15,61 +20,31 @@ ALLOWED_ERROR = (0.013042655732193589, 0.4634594642907113)  # eco
 ALLOWED_ERROR = (0.022824315312188172, 0.10965274847082523) # renewable
 ALLOWED_ERROR = (0.01101716635261566, 0.5170811024214211) # load
 
-def main(optuna_path: str, other_paths: tuple, show_all=True, store=True):
-    # Load the data
-    optuna_df = load_optuna_csv(optuna_path)
-
-    other_dfs = []
-    for other_path in other_paths:
-        other_dfs.append(load_drl_csv(other_path))
-
-    # Plot the data
+def plot_pareto_plus(optuna_df: str | pd.DataFrame, baseline_paths: tuple, show_all=True, store=True):
+    """ Plot pareto front plus some baseline experiments """
     metrics = ['values_Invalid share', 'values_Mean error']
-    default_metrics = ['values_' + str(m) for m in range(len(metrics))]
+    if isinstance(optuna_df, str):
+        optuna_path = optuna_df
+        optuna_df = load_optuna_data(optuna_path, metrics)
 
-    try:
-        optuna_df[metrics[0]] = optuna_df[default_metrics[0]]
-        optuna_df[metrics[1]] = optuna_df[default_metrics[1]]
-    except KeyError:
-        pass
-
-    # Remove NaN values and reset the index
-    optuna_df = optuna_df.dropna(subset=list(metrics))
-    optuna_df = optuna_df.reset_index(drop=True)
-
-    # print(optuna_df)
-
-    # # Create columns of normalized metrics
-    # normalized_metrics = [m + '_normalized' for m in metrics]
-    # for metric, normalized_metric in zip(metrics, normalized_metrics):
-    #     optuna_df[normalized_metric] = (optuna_df[metric] - optuna_df[metric].min()) / (
-    #             optuna_df[metric].max() - optuna_df[metric].min())
-
-    optuna_df['non_dominated'] = pareto_front.compute_pareto_front(optuna_df[metrics])
-    optuna_df['fuzzy_dominated'] = pareto_front.compute_fuzzy_dominance(optuna_df[metrics], optuna_df['non_dominated'], ALLOWED_ERROR)
+    baseline_dfs = load_baseline_data(baseline_paths)
 
     fig = plt.figure()
     # Plot dominated solutions
     if show_all:
-        for idx, row in enumerate(optuna_df[~optuna_df.non_dominated].iterrows()):
-            label = 'Dominated' if idx==0 else None
-            plt.plot(row[1][metrics[0]], row[1][metrics[1]], 'bo', label=label)
+        plot_dominated_points(optuna_df, metrics)
 
     # Plot fuzzy dominated solutions
-    for idx, row in enumerate(optuna_df[optuna_df.fuzzy_dominated].iterrows()):
-        label = 'Fuzzy-dominated' if idx==0 else None
-        plt.plot(row[1][metrics[0]], row[1][metrics[1]], 'yo', label=label)
+    plot_fuzzy_dominated_points(optuna_df, metrics)
 
     # Plot Pareto front solutions
-    for idx, row in enumerate(optuna_df[optuna_df.non_dominated].iterrows()):
-        label = 'Non-dominated' if idx==0 else None
-        plt.plot(row[1][metrics[0]], row[1][metrics[1]], 'ro', label=label)
+    plot_pareto_points(optuna_df, metrics)
 
     # Plot other experiments as comparison
-    for idx, other_df in enumerate(other_dfs):
+    for idx, baseline_df in enumerate(baseline_dfs):
         label = 'Manual' if idx==0 else None
-        invalid_share = 1 - other_df['valid_share_possible'].mean()
-        valid_regret = other_df['valid_regret'].mean()
+        invalid_share = 1 - baseline_df['valid_share_possible'].mean()
+        valid_regret = baseline_df['valid_regret'].mean()
         plt.plot(invalid_share, valid_regret, 'go', label=label)
 
     plt.xlabel('Invalid share')
@@ -83,7 +58,143 @@ def main(optuna_path: str, other_paths: tuple, show_all=True, store=True):
     plt.close(fig)
 
 
-if __name__ == '__main__':
-    env = 'load'
-    main(f'data/20240906_{env}_multi', (f'data/comparison2/{env}_default_50k_09',f'data/comparison2/{env}_default_50k'))
+def plot_annotated(optuna_df: str | pd.DataFrame, top_n: int=8, store_as_tiks=False):
+    """ Plot pareto front, highlight areas of interest (constraint focus,
+    objective focus, utopia), and annotate the areas with the the prevalent
+    environment design decisions. """
 
+    metrics = ['values_Invalid share', 'values_Mean error']
+    if isinstance(optuna_df, str):
+        optuna_df = load_optuna_data(optuna_df, metrics)
+
+    # Plot pareto front data points
+    plot_dominated_points(optuna_df, metrics)
+    plot_pareto_points(optuna_df, metrics)
+
+    ax = plt.gca()
+
+    # Highlight and annotate areas of interest
+    # Constraint focus: Vertical line to highlight the min top_n data points
+    top_df, bottom_df = get_best_design.filter_by_constraints(optuna_df, top_n)
+    plt.axvline(x=top_df[metrics[0]].max(), color='g', linestyle='--')
+    x_position = top_df[metrics[0]].min() + (top_df[metrics[0]].max() - top_df[metrics[0]].min()) / 2
+    y_position = ax.get_ylim()[0] + (ax.get_ylim()[1] - ax.get_ylim()[0]) / 2
+    add_annotation_to_plot('Constraints:', x_position, y_position, top_df, bottom_df)
+
+    # Objective focus: Horizontal line to highlight the min top_n data points
+    top_df, bottom_df = get_best_design.filter_by_objective(optuna_df, top_n)
+    plt.axhline(y=top_df[metrics[1]].max(), color='r', linestyle='--')
+    x_position = ax.get_xlim()[0] + (ax.get_xlim()[1] - ax.get_xlim()[0]) / 2
+    y_position = top_df[metrics[1]].min() + (top_df[metrics[1]].max() - top_df[metrics[1]].min()) / 2
+    add_annotation_to_plot('Objective:', x_position, y_position, top_df, bottom_df)
+
+    # Utopia focus: box in bottom left to highlight the min top_n data points
+    top_df, bottom_df = get_best_design.filter_by_utopia(optuna_df, top_n)
+    vertices = (
+        (-100, top_df[metrics[1]].max()),  # Top left
+        (top_df[metrics[0]].max(), top_df[metrics[1]].max()),  # Top right
+        (top_df[metrics[0]].max(), -100)  # Bottom right
+    )
+    ax.add_patch(patches.Polygon(vertices, closed=False, edgecolor='blue', facecolor='none', linestyle='--'))
+    x_position = ax.get_xlim()[0] + (ax.get_xlim()[1] - ax.get_xlim()[0]) / 10
+    y_position = ax.get_ylim()[0] + (ax.get_ylim()[1] - ax.get_ylim()[0]) / 10
+    add_annotation_to_plot('Utopia:', x_position, y_position, top_df, bottom_df)
+
+    # TODO: Pareto annotation?! Where to place?!
+    top_df, bottom_df = get_best_design.filter_by_pareto(optuna_df)
+    x_position = ax.get_xlim()[0] + (ax.get_xlim()[1] - ax.get_xlim()[0]) / 2
+    y_position = ax.get_ylim()[0] + (ax.get_ylim()[1] - ax.get_ylim()[0]) / 2
+    add_annotation_to_plot('Pareto:', x_position, y_position, top_df, bottom_df)
+
+
+    # General plotting stuff
+    plt.xlabel('Invalid share')
+    plt.ylabel('Mean valid error')
+    plt.legend()
+
+    if store_as_tiks:
+        tikzplotlib.save("plot.tikz")
+    else:
+        plt.show()
+
+
+def load_baseline_data(baseline_paths):
+    baseline_dfs = []
+    for other_path in baseline_paths:
+        baseline_dfs.append(load_drl_csv(other_path))
+
+    return baseline_dfs
+
+
+def load_optuna_data(optuna_path, metrics):
+    optuna_df = load_optuna_csv(optuna_path)
+
+    default_metrics = ['values_' + str(m) for m in range(len(metrics))]
+    try:
+        optuna_df[metrics[0]] = optuna_df[default_metrics[0]]
+        optuna_df[metrics[1]] = optuna_df[default_metrics[1]]
+    except KeyError:
+        pass
+
+    # Remove NaN values and reset the index
+    optuna_df = optuna_df[optuna_df.state == 'COMPLETE']
+    optuna_df = optuna_df.dropna(subset=list(metrics))
+    optuna_df = optuna_df.reset_index(drop=True)
+
+    optuna_df = add_normalized_metrics_columns(optuna_df, metrics)
+
+    optuna_df['non_dominated'] = pareto_front.compute_pareto_front(optuna_df[metrics])
+    optuna_df['fuzzy_dominated'] = pareto_front.compute_fuzzy_dominance(optuna_df[metrics], optuna_df['non_dominated'], ALLOWED_ERROR)
+
+    return optuna_df
+
+
+def add_normalized_metrics_columns(df, metrics):
+    for metric in metrics:
+        df[metric + '_normalized'] = (df[metric] - df[metric].min()) / (
+                df[metric].max() - df[metric].min())
+    return df
+
+
+def plot_dominated_points(full_df, metrics):
+    plot_data_points(full_df[~full_df.non_dominated], metrics, 'bo', 'Dominated')
+
+def plot_fuzzy_dominated_points(full_df, metrics):
+    plot_data_points(full_df[full_df.fuzzy_dominated], metrics, 'yo', 'Fuzzy-dominated')
+
+def plot_pareto_points(full_df, metrics):
+    plot_data_points(full_df[full_df.non_dominated], metrics, 'ro', 'Non-dominated')
+
+
+def plot_data_points(df_to_plot, metrics, style='bo', label=None):
+    """ Scatter plot of the data points """
+    for idx, row in enumerate(df_to_plot.iterrows()):
+        label = label if idx==0 else None
+        plt.plot(row[1][metrics[0]], row[1][metrics[1]], style, label=label)
+
+
+def add_annotation_to_plot(header: str, x_position: float, y_position: float, top_df, bottom_df):
+    best_design_continuous, best_design_discrete = get_best_design.get_best_design(top_df, bottom_df)
+    annotation = extract_significant_design_decisions_as_str(best_design_continuous)
+    annotation += extract_significant_design_decisions_as_str(best_design_discrete)
+    if annotation == '':
+        annotation = 'No significant design decisions'
+    else:
+        annotation = header + '\n' + annotation
+    plt.text(x_position, y_position, annotation, fontsize=8, color='k', ha='center', va='center')
+
+
+def extract_significant_design_decisions_as_str(df):
+    """ Extract the significant design decisions from the data frame """
+    text = ''
+    for idx, row in df.iterrows():
+        significant = row['Significant']
+        if significant:
+            text += f'{row["Parameter"][7:]}\n'
+    return text
+
+
+if __name__ == '__main__':
+    env = 'renewable'
+    # plot_pareto_plus(f'data/20240906_{env}_multi', (f'data/comparison2/{env}_default_50k_09',f'data/comparison2/{env}_default_50k'))
+    plot_annotated(f'HPC/auto_env_design/data/20241104_multi_GA/{env}_new_dist_pwl_1step/')
