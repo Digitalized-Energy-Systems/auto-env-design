b29ba4f92c3d77a0d3b483c8c1cdf19fd2a6b566
Thomas Wolgast <thomas.wolgast@uni-oldenburg.de>
2024-12-05 13:28:19 +0100

    Simplify std dev compuation with new folder structure

--- get_metric_variance.py
+++ get_metric_variance.py
@@ -9,61 +9,60 @@ import pandas as pd
 from util import load_drl_csv
 
 
-def get_metric_variance(path: str):
-    all_errors = collections.defaultdict(list)
-    all_invalids = collections.defaultdict(list)
+def get_metric_std_dev(path: str):
+    all_errors = []
+    all_invalids = []
 
-    for sub_path in os.listdir(path):
-        full_path = os.path.join(path, sub_path)
-
-        if not os.path.isdir(full_path):
+    for sample_path in os.listdir(path):
+        sample_errors = []
+        sample_invalid_shares = []
+        sample_path = os.path.join(path, sample_path)
+        if not os.path.isdir(sample_path):
             continue
 
-        print(f'Processing {full_path}')
+        # print(f'Processing {sample_path}')
+        for run_path in os.listdir(sample_path):
+            full_path = os.path.join(sample_path, run_path)
 
-        try:
-            df = load_drl_csv(full_path)
-        except (FileNotFoundError, NotADirectoryError):
-            continue
+            if not os.path.isdir(full_path):
+                continue
 
-        mean_error = df['valid_regret'].mean()
-        mean_invalids = 1 - df['valid_share_possible'].mean()
+            try:
+                df = load_drl_csv(full_path)
+            except (FileNotFoundError, NotADirectoryError):
+                continue
 
-        # Open the env_hyperparams.json as string
-        with open(os.path.join(full_path, 'env_hyperparams.json')) as f:
-            env_hyperparams = str(json.load(f))
+            sample_errors.append(df['valid_regret'].mean())
+            sample_invalid_shares.append(1 - df['valid_share_possible'].mean())
 
-        all_errors[env_hyperparams].append(mean_error)
-        all_invalids[env_hyperparams].append(mean_invalids)
+        # Compute standard dev within the n runs with same hyperparams
+        all_errors.append(np.std(sample_errors))
+        all_invalids.append(np.std(sample_invalid_shares))
 
-    all_errors = list(all_errors.values())
-    all_invalids = list(all_invalids.values())
+    mean_std_invalids = np.nanmean(all_invalids)
+    mean_std_error = np.nanmean(all_errors)
 
-    # Compute standard deviation per metric for all experiments
-    std_error = [np.std(l) for l in all_errors]
-    mean_std_error = np.mean([std for std in std_error if not np.isnan(std)])
-    std_invalids = [np.std(l) for l in all_invalids]
-    mean_std_invalids = np.mean([std for std in std_invalids if not np.isnan(std)])
+    # print(f'Mean std error: {mean_std_error}')
+    # print(f'Mean std invalid share: {mean_std_invalids}')
+    # print('standard deviations error:', (mean_std_invalids, mean_std_error))
 
-    print(f'Mean std error: {mean_std_error}')
-    print(f'Mean std invalid share: {mean_std_invalids}')
-    print('standard deviations error:', (mean_std_invalids, mean_std_error))
+    # # Compute min and max per metric for all experiments
+    # min_error = np.min([np.mean(l) for l in all_errors if not np.isnan(np.mean(l))])
+    # max_error = np.max([np.mean(l) for l in all_errors if not np.isnan(np.mean(l))])
+    # min_invalids = np.min([np.mean(l) for l in all_invalids if not np.isnan(np.mean(l))])
+    # max_invalids = np.max([np.mean(l) for l in all_invalids if not np.isnan(np.mean(l))])
 
-    # Compute min and max per metric for all experiments
-    min_error = np.min([np.mean(l) for l in all_errors if not np.isnan(np.mean(l))])
-    max_error = np.max([np.mean(l) for l in all_errors if not np.isnan(np.mean(l))])
-    min_invalids = np.min([np.mean(l) for l in all_invalids if not np.isnan(np.mean(l))])
-    max_invalids = np.max([np.mean(l) for l in all_invalids if not np.isnan(np.mean(l))])
+    # # Compute relative std per metric for all experiments
+    # rel_std_error = mean_std_error / (max_error - min_error)
+    # rel_std_invalids = mean_std_invalids / (max_invalids - min_invalids)
 
-    # Compute relative std per metric for all experiments
-    rel_std_error = mean_std_error / (max_error - min_error)
-    rel_std_invalids = mean_std_invalids / (max_invalids - min_invalids)
-
-    print(f'Relative std error: {rel_std_error}')
-    print(f'Relative std invalid share: {rel_std_invalids}')
+    # print(f'Relative std error: {rel_std_error}')
+    # print(f'Relative std invalid share: {rel_std_invalids}')
 
     # Compute std per metric only for non-dominated runs
     # TODO
+
+    return mean_std_invalids, mean_std_error
 
 
 if __name__ == '__main__':
