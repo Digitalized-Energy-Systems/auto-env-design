d7cf7961a283b4f760488de64f035b6699e87994
Thomas Wolgast <thomas.wolgast@uni-oldenburg.de>
2025-05-01 11:29:01 +0200

    Final hyperparameter search space definition

--- src/optuna_experiment.py
+++ src/optuna_experiment.py
@@ -80,36 +80,16 @@ def opf_env_design_sampling(trial):
     # hps['train_data'] = trial.suggest_categorical('train_data', ["mixed"])
     hps['add_res_obs'] = 'mixed'  # trial.suggest_categorical('add_res_obs', ['mixed'])
     hps['add_mean_obs'] = trial.suggest_categorical('add_mean_obs', [True, False])
-    hps['add_act_obs'] = trial.suggest_categorical('add_act_obs', [True, False])
     hps['autoscale_actions'] = trial.suggest_categorical('autoscale_actions', [True, False])
     hps['diff_objective'] = trial.suggest_categorical('diff_objective', [True, False])
-    hps['steps_per_episode'] = trial.suggest_int('steps_per_episode', 1, 5, step=2)
-
-    # Diff action space only possible for multi-step
-    hps['diff_action_space'] = trial.suggest_categorical('diff_action_space', [True, False])
-    if hps['steps_per_episode'] == 1:
-        hps['diff_action_space'] = False
-    elif hps['diff_action_space']:
-        hps['diff_action_step_size'] = 2 / hps['steps_per_episode']
-    # hps['initial_action'] = trial.suggest_categorical('initial_action', ['center', 'random'])
-    hps['only_worst_case_violations'] = trial.suggest_categorical('only_worst_case_violations', [True, False])
-
-    # Penalty function
-    hps['penalty_fct'] = trial.suggest_categorical('penalty_fct', ['linear', 'quadr', 'sqrt'])
-    linear_penalty = 0
-    quadr_penalty = 0
-    sqrt_penalty = 0
-    if hps['penalty_fct'] == 'linear':
-        linear_penalty = 1
-    elif hps['penalty_fct'] == 'quadr':
-        quadr_penalty = 1
-    elif hps['penalty_fct'] == 'sqrt':
-        sqrt_penalty = 1
-    # Simplification: Same setting for all constraints!
-    hps['volt_pen_kwargs'] = {'linear_penalty': linear_penalty, 'quadr_penalty': quadr_penalty, 'sqrt_penalty': sqrt_penalty}
-    hps['trafo_pen_kwargs'] = {'linear_penalty': linear_penalty, 'quadr_penalty': quadr_penalty, 'sqrt_penalty': sqrt_penalty}
-    hps['line_pen_kwargs'] = {'linear_penalty': linear_penalty, 'quadr_penalty': quadr_penalty, 'sqrt_penalty': sqrt_penalty}
-    hps['ext_grid_pen_kwargs'] = {'linear_penalty': linear_penalty, 'quadr_penalty': quadr_penalty, 'sqrt_penalty': sqrt_penalty}
+    
+    # Constraints
+    hps['constraint_params'] = {}
+    hps['worst_case_violations'] = trial.suggest_categorical('only_worst_case_violations', [True, False])
+    hps['constraint_params']['only_worst_case_violations'] = hps['only_worst_case_violations']
+    penalty_fct_options = [0.5, 1.0, 2.0]
+    hps['penalty_index'] = trial.suggest_int('penalty_index', 0, 2)
+    hps['constraint_params']['penalty_power'] = penalty_fct_options[hps['penalty_index']]
 
     # Addidional observations
     hps['add_voltage_magnitude'] = trial.suggest_categorical('add_voltage_magnitude', [True, False])
@@ -145,27 +125,48 @@ def opf_env_design_sampling(trial):
     simbench = hps['simbench_share']
     uniform = simbench + hps['uniform_share']
     normal = uniform + hps['normal_share']
-    hps['sampling_kwargs'] = dict()
-    hps['sampling_kwargs']['data_probabilities'] = (simbench, uniform, normal)
+    hps['sampling_params'] = dict()
+    hps['sampling_params']['data_probabilities'] = (simbench, uniform, normal)
     hps['noise_factor'] = trial.suggest_float('noise_factor', 0, 0.2)
-    hps['sampling_kwargs']['noise_factor'] = hps['noise_factor']
+    hps['sampling_params']['noise_factor'] = hps['noise_factor']
+    hps['sampling_params']['interpolate_steps'] = trial.suggest_categorical('interpolate_steps', [True, False])
+    hps['interpolate_steps'] = hps['sampling_params']['interpolate_steps']
+    hps['train_data'] = 'mixed'
 
-    hps['penalty_weight'] = trial.suggest_float('penalty_weight', 0.01, 0.99)
+    # Define episode / action
+    # if hps['add_n_obs_channels'] > 1:  # Assumption at least two additional obs required
+    #     hps['steps_per_episode'] = trial.suggest_int('steps_per_episode', 1, 5, step=2)
+    # else:
+    #     hps['steps_per_episode'] = trial.suggest_int('steps_per_episode', 1, 1, step=2)
+    # hps['add_act_obs'] = True if hps['steps_per_episode'] > 1 else False
+
+    # Diff action space only possible for multi-step
+    # hps['diff_action_space'] = trial.suggest_categorical('diff_action_space', [True, False])
+    # if hps['diff_action_space'] and hps['steps_per_episode']:
+    #     hps['diff_action_step_size'] = 2 / hps['steps_per_episode']
+    # else:
+    #     hps['diff_action_step_size'] = None
+    # hps['initial_action'] = trial.suggest_categorical('initial_action', ['center', 'random'])
+
     hps['clipped_action_penalty'] = trial.suggest_float('clipped_action_penalty', 0, 10)
 
     # Reward function params
+    hps['penalty_weight'] = trial.suggest_float('penalty_weight', 0.01, 0.99)
     hps['valid_reward'] = trial.suggest_float('valid_reward', 0.0, 2.0)
     hps['invalid_penalty'] = trial.suggest_float('invalid_penalty', 0.0, 2.0)
-    hps['objective_share'] = trial.suggest_float('objective_share', 0.0, 1.0)
+    hps['invalid_objective_share'] = trial.suggest_float('invalid_objective_share', 0.0, 1.0)
     hps['reward_function_params'] = dict()
+    hps['reward_function_params']['penalty_weight'] = hps['penalty_weight']
     hps['reward_function_params']['valid_reward'] = hps['valid_reward']
     hps['reward_function_params']['invalid_penalty'] = hps['invalid_penalty']
-    hps['reward_function_params']['objective_share'] = hps['objective_share']
+    hps['reward_function_params']['invalid_objective_share'] = hps['invalid_objective_share']
+    hps['reward_function_params']['reward_scaling'] = 'normalization'
 
     return hps
 
 
 def opf_env_design_sampling_safe(trial):
+    assert False, 'Removed for now. Maybe in later publication again?!'
     # Focus only on the design decisions relevant for the cost function for safe RL
     hps = dict()
 
